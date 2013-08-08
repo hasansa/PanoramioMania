695722641c842527925f9df2cd6741103e76376f
Hasan_Sawaed <Hasan_Sawaed@Hasan-Sawaeds-MacBook-Pro.local>
2013-08-08 10:29:21 +0300

    Initial Commit

---
+++ PanoramioMania/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PanoramioMania
+//
+//  Created by Hasan_Sawaed on 8/8/13.
+//  Copyright (c) 2013 Hasan_Sawaed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PanoramioMania/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PanoramioMania
+//
+//  Created by Hasan_Sawaed on 8/8/13.
+//  Copyright (c) 2013 Hasan_Sawaed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
